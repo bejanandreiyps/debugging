b73789339c9349cfa5dc27bfd0290599d1d2ce47
bejanandreiyps <andrei.bejan@yopeso.com>
2022-07-14 22:27:54 +0300

    Fixed static TextView, fixed division function.

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -9,18 +9,22 @@ import android.widget.TextView
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val helloTextView: TextView = findViewById(R.id.hello_world)
-        helloTextView.text = "Hello, debugging!"
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "this is where the app crashed before")
+        val helloTextView: TextView = findViewById(R.id.hello_world)
+        Log.d(TAG, "this should be logged if the bug is fixed")
+        helloTextView.text = "Hello, debugging!"
+        logging()
         division()
     }
 
     fun division() {
         val numerator = 60
         var denominator = 4
-        repeat(5) {
+        repeat(4) {
             Log.d(TAG, "$denominator")
             Log.v(TAG, "${numerator / denominator}")
             denominator--

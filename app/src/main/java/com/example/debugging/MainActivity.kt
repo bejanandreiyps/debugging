44f71fc14cdcd87a2a93fe94794ec7ec647da2f0
bejanandreiyps <andrei.bejan@yopeso.com>
2022-07-14 20:48:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.debugging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

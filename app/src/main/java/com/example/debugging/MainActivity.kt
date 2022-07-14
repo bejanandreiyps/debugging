802d0323e285e154a48d1f8e35d3b2932a760a2d
bejanandreiyps <andrei.bejan@yopeso.com>
2022-07-14 21:56:48 +0300

    Assigned a value that doesn't exist.

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.debugging
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val helloTextView: TextView = findViewById(R.id.hello_world)
+        helloTextView.text = "Hello, debugging!"
         setContentView(R.layout.activity_main)
-        logging()
         division()
     }
 
@@ -18,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         val numerator = 60
         var denominator = 4
         repeat(5) {
+            Log.d(TAG, "$denominator")
             Log.v(TAG, "${numerator / denominator}")
             denominator--
         }
